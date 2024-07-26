e11739efa857d56c3bd7db356e4d0e04cd5088c2
Shogo Ishigami <29514220+nusu-github@users.noreply.github.com>
2024-07-27 01:04:20 +0900

    前処理と後処理を分けた

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,19 @@ use std::{fs, path::PathBuf};
 
 use anyhow::{ensure, Context, Result};
 use clap::Parser;
-use crossbeam_channel::{bounded, Receiver, Sender};
-use image::{ImageFormat, RgbImage, RgbaImage};
-use indicatif::{ProgressBar, ProgressFinish, ProgressStyle};
+use crossbeam_channel::{bounded, unbounded};
+use image::ImageFormat;
+use indicatif::{MultiProgress, ProgressBar, ProgressFinish, ProgressStyle};
 use walkdir::WalkDir;
 
-use crate::model::Model;
+use crate::imageops_ai::clip_minimum_border::clip_minimum_border;
+use crate::imageops_ai::mask::apply_mask;
+use crate::model::{postprocess_mask, preprocess, Model};
 
 mod imageops_ai;
 mod model;
 
-#[derive(Parser)]
+#[derive(Parser, Clone)]
 #[command(version, about, long_about = None)]
 struct Config {
     input_dir: PathBuf,
@@ -23,10 +25,8 @@ struct Config {
     format: String,
     #[arg(short, long, default_value_t = 0)]
     device_id: i32,
-    #[arg(short, long, default_value_t = std::thread::available_parallelism().unwrap().get())]
+    #[arg(short, long, default_value_t = 1)]
     batch_size: usize,
-    #[arg(long, default_value_t = std::thread::available_parallelism().unwrap().get())]
-    thread_size: usize,
 }
 
 fn main() -> Result<()> {
@@ -36,6 +36,7 @@ fn main() -> Result<()> {
     ensure!(config.input_dir.exists(), "Input directory does not exist");
 
     let model = Model::new(&config.model_path, config.device_id)?;
+    let image_size = model.image_size;
 
     let image_paths: Vec<_> = WalkDir::new(&config.input_dir)
         .into_iter()
@@ -43,28 +44,44 @@ fn main() -> Result<()> {
         .filter(|e| ImageFormat::from_path(e.path()).is_ok())
         .map(|e| e.into_path())
         .collect();
+    let batched_paths: Vec<_> = image_paths.chunks(config.batch_size).collect();
 
-    let progress_bar =
-        ProgressBar::new(image_paths.len() as u64).with_finish(ProgressFinish::Abandon);
-    progress_bar.set_style(ProgressStyle::default_bar().template(
-        "{spinner:.green} [{elapsed}] [{bar:40.cyan/blue}] {pos}/{len} ({per_sec} {eta})",
-    )?);
+    let (load_bar, inference_bar, output_bar) = progress(image_paths.len() as u64)?;
 
-    let (load_tx, load_rx): (Sender<(RgbImage, PathBuf)>, Receiver<(RgbImage, PathBuf)>) =
-        bounded(config.thread_size);
-    let (inference_tx, inference_rx): (
-        Sender<(RgbaImage, PathBuf)>,
-        Receiver<(RgbaImage, PathBuf)>,
-    ) = bounded(config.thread_size);
+    let (load_tx, load_rx) = bounded(config.batch_size);
+    let (inference_tx, inference_rx) = bounded(config.batch_size);
 
     rayon::scope(|s| {
         s.spawn(move |s| {
-            for path in image_paths {
+            for paths in batched_paths {
                 let load_tx = load_tx.clone();
-                s.spawn(move |_| {
-                    let image = image::open(&path).unwrap().into_rgb8();
-                    load_tx.send((image, path)).unwrap();
-                })
+
+                let (result_tx, result_rx) = unbounded();
+                paths.into_iter().enumerate().for_each(|(i, path)| {
+                    s.spawn({
+                        let result_tx = result_tx.clone();
+                        let load_bar = load_bar.clone();
+                        move |_| {
+                            load_bar.set_message(path.display().to_string());
+                            let image = image::open(&path).unwrap().into_rgb8();
+                            let (tensor, crop) = preprocess(&image, image_size);
+                            result_tx
+                                .send((i, (image, path.clone(), tensor, crop)))
+                                .unwrap();
+
+                            load_bar.inc(1);
+                        }
+                    })
+                });
+                drop(result_tx);
+
+                let mut batch: Vec<_> = result_rx.iter().collect();
+                batch.sort_by(|a, b| a.0.cmp(&b.0));
+                let batch = batch.into_iter().map(|(_, x)| x).collect::<Vec<_>>();
+
+                for (image, path, tensor, crop) in batch.into_iter() {
+                    load_tx.send((image, path, tensor, crop)).unwrap();
+                }
             }
         });
 
@@ -74,34 +91,70 @@ fn main() -> Result<()> {
                 .build()
                 .unwrap();
 
-            while let Ok((image, path)) = load_rx.recv() {
+            while let Ok((image, path, tensor, crop)) = load_rx.recv() {
                 let inference_tx = inference_tx.clone();
-                let model = model.clone();
+                let inference_bar = inference_bar.clone();
                 pool.spawn(move || {
-                    let inferred_image = model.predict(&image).unwrap();
-                    inference_tx.send((inferred_image, path)).unwrap();
+                    let mask = model.predict(tensor.view()).unwrap();
+                    inference_tx.send((image, path, mask, crop)).unwrap();
+
+                    inference_bar.inc(1);
                 })
             }
         });
 
         s.spawn(move |s| {
-            while let Ok((image, path)) = inference_rx.recv() {
+            while let Ok((image, path, mask, crop)) = inference_rx.recv() {
                 let relative_path = path.strip_prefix(&config.input_dir).unwrap();
                 let output_path = config
                     .output_dir
                     .join(relative_path)
                     .with_extension(&config.format);
                 s.spawn(move |_| {
+                    let (width, height) = image.dimensions();
+                    let mask = postprocess_mask(mask.view(), image_size, crop, width, height);
+                    let image = apply_mask(&image, &mask, true).unwrap();
+                    let image = clip_minimum_border(image, 1, 8);
+
                     fs::create_dir_all(output_path.parent().unwrap()).unwrap();
                     image
                         .save(&output_path)
                         .with_context(|| format!("Failed to save image: {}", output_path.display()))
                         .unwrap();
                 });
-                progress_bar.inc(1);
+
+                output_bar.inc(1);
             }
         });
     });
 
     Ok(())
+}
+
+fn progress(image_paths_count: u64) -> Result<(ProgressBar, ProgressBar, ProgressBar)> {
+    let mp = MultiProgress::new();
+
+    let load_bar = mp
+        .add(ProgressBar::new(image_paths_count))
+        .with_finish(ProgressFinish::AbandonWithMessage("Completed!".into()));
+    load_bar.set_style(ProgressStyle::default_bar()
+        .template("Preprocessing: {spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} ({eta}) {msg}")?
+        .progress_chars("#>-"));
+
+    let inference_bar = mp
+        .add(ProgressBar::new(image_paths_count))
+        .with_finish(ProgressFinish::AbandonWithMessage("Completed!".into()));
+    inference_bar.set_style(ProgressStyle::default_bar()
+        .template("Inference: {spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} ({eta})")?
+        .progress_chars("#>-"));
+
+    let output_bar = mp
+        .add(ProgressBar::new(image_paths_count))
+        .with_finish(ProgressFinish::AbandonWithMessage("Completed!".into()));
+    output_bar.set_style(ProgressStyle::default_bar()
+        .template("Output: {spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} ({eta})")?
+        .progress_chars("#>-"));
+
+    mp.set_move_cursor(true);
+    Ok((load_bar, inference_bar, output_bar))
 }
--- src/model.rs
+++ src/model.rs
@@ -3,22 +3,18 @@ use std::{path::Path, sync::OnceLock};
 use anyhow::Result;
 use image::{
     imageops::FilterType, DynamicImage, GenericImageView, ImageBuffer, Luma, Rgb, RgbImage,
-    RgbaImage,
 };
 use ndarray::prelude::*;
 use ort::Session;
 
-use crate::imageops_ai::{
-    clip_minimum_border::clip_minimum_border,
-    mask::apply_mask,
-    padding::{get_position, padding_square, Position},
-};
+use crate::imageops_ai::padding::{get_position, padding_square, Position};
 
 static SESSION: OnceLock<Session> = OnceLock::new();
 
 #[derive(Copy, Clone)]
 pub struct Model {
-    image_size: u32,
+    pub image_size: u32,
+
     session: &'static Session,
 }
 
@@ -39,58 +35,45 @@ impl Model {
         })
     }
 
-    pub fn predict(&self, image: &RgbImage) -> Result<RgbaImage> {
-        let (width, height) = image.dimensions();
-        let (tensor, crop) = self.preprocess(image);
-
-        let outputs = self.session.run(ort::inputs!["img" => tensor.view()]?)?;
-        let mask = outputs["mask"]
+    pub fn predict(&self, tensor: ArrayView4<f32>) -> Result<Array4<f32>> {
+        let outputs = self.session.run(ort::inputs!["img" => tensor]?)?;
+        Ok(outputs["mask"]
             .try_extract_tensor::<f32>()?
-            .into_dimensionality::<Ix4>()?;
-        let mask = self.postprocess_mask(mask, crop, width, height);
-
-        let image = apply_mask(image, &mask, true).unwrap();
-        Ok(clip_minimum_border(image, 1, 8))
+            .into_dimensionality::<Ix4>()?
+            .to_owned())
     }
+}
 
-    fn preprocess(&self, image: &RgbImage) -> (Array4<f32>, [u32; 4]) {
-        let image = image::imageops::resize(
-            image,
-            self.image_size,
-            self.image_size,
-            FilterType::Lanczos3,
-        );
-        let (w, h) = image.dimensions();
-        let (x, y) = get_position(w, h, self.image_size, self.image_size, Position::Center)
-            .unwrap_or_default();
-        let image = padding_square(&image, Position::Center, Rgb([0, 0, 0])).unwrap();
-        let image = DynamicImage::from(image).into_rgb32f();
+pub fn preprocess(image: &RgbImage, image_size: u32) -> (Array4<f32>, [u32; 4]) {
+    let image = image::imageops::resize(image, image_size, image_size, FilterType::Lanczos3);
+    let (w, h) = image.dimensions();
+    let (x, y) = get_position(w, h, image_size, image_size, Position::Center).unwrap_or_default();
+    let image = padding_square(&image, Position::Center, Rgb([0, 0, 0])).unwrap();
+    let image = DynamicImage::from(image).into_rgb32f();
 
-        let tensor = unsafe {
-            ArrayView3::from_shape_ptr(
-                (self.image_size as usize, self.image_size as usize, 3),
-                image.as_ptr(),
-            )
-        }
-        .permuted_axes([2, 0, 1])
-        .slice(s![NewAxis, .., .., ..])
-        .into_owned();
-
-        (tensor, [x as u32, y as u32, w, h])
+    let tensor = unsafe {
+        ArrayView3::from_shape_ptr(
+            (image_size as usize, image_size as usize, 3),
+            image.as_ptr(),
+        )
     }
+    .permuted_axes([2, 0, 1])
+    .slice(s![NewAxis, .., .., ..])
+    .into_owned();
 
-    fn postprocess_mask(
-        &self,
-        mask: ArrayView4<f32>,
-        crop: [u32; 4],
-        width: u32,
-        height: u32,
-    ) -> ImageBuffer<Luma<f32>, Vec<f32>> {
-        let mask = mask.slice(s![0, 0, .., ..]).to_owned();
-        let [x, y, w, h] = crop;
-        let mask =
-            ImageBuffer::from_raw(self.image_size, self.image_size, mask.into_raw_vec()).unwrap();
-        let mask = mask.view(x, y, w, h).to_image();
-        image::imageops::resize(&mask, width, height, FilterType::Lanczos3)
-    }
+    (tensor, [x as u32, y as u32, w, h])
+}
+
+pub fn postprocess_mask(
+    mask: ArrayView4<f32>,
+    image_size: u32,
+    crop: [u32; 4],
+    width: u32,
+    height: u32,
+) -> ImageBuffer<Luma<f32>, Vec<f32>> {
+    let mask = mask.slice(s![0, 0, .., ..]).to_owned();
+    let [x, y, w, h] = crop;
+    let mask = ImageBuffer::from_raw(image_size, image_size, mask.into_raw_vec()).unwrap();
+    let mask = mask.view(x, y, w, h).to_image();
+    image::imageops::resize(&mask, width, height, FilterType::Lanczos3)
 }
